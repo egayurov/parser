3113b156beded846d9fabab1307a7af8a5e45ca9
egayurov <101916014+egayurov@users.noreply.github.com>
2026-06-10 22:06:58 +0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -42,10 +42,10 @@ def main():
         browser = p.chromium.launch(headless=True)
         context = browser.new_context(
             proxy=proxy_settings,
+            ignore_https_errors=True,  # <--- ВОТ ЭТА ВОЛШЕБНАЯ СТРОЧКА
             viewport={'width': 1920, 'height': 1080},
             user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
         )
-        page = context.new_page()
 
         print(f"[*] Открываем раздел продажа квартир...")
         try:
